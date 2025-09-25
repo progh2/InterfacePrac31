e04632ddf161e87e4ec43a91904040b7fb8a3a0f
Gihun Ham <progh2@gmail.com>
2025-09-25 09:39:13 +0900

    Product 비교 - IComparable 인터페이스를 상속해서 CompareTo 메서드 구현

--- Program.cs
+++ Program.cs
@@ -8,8 +8,43 @@ namespace InterfacePrac31
 {
     internal class Program
     {
+
+        class Product : IComparable<Product>
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(Product other)
+            {
+                //return this.Price - other.Price;
+                //return this.Price.CompareTo(other.Price);
+                return this.Name.CompareTo(other.Name);
+            }
+
+            public override string ToString()
+            {
+                //return Name + " : " + Price + "원";
+                   return $"{Name} : {Price}원";
+            }
+        }
+
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Name="감자", Price =500},
+                new Product(){Price=700, Name="사과" },
+                new Product(){Name="고구마", Price =400},
+                new Product(){Name="배추", Price =600},
+                new Product(){Name="상추", Price =300},
+            };
+            //정렬
+            products.Sort();
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
